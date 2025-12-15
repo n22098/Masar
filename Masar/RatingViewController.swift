16e26cbea6d8d9bf151ebabd0fb5ac68a12af416
n22098 <202201259@student.polytechnic.bh>
2025-12-15 18:28:38 +0300

    .......

---
+++ Masar/RatingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RatingViewController.swift
+//  Masar
+//
+//  Created by BP-36-201-09 on 15/12/2025.
+//
+
+import UIKit
+
+class RatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
