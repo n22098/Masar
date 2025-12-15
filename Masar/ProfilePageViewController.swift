bd3540340e331671eedf7263daffc5d7b69946f2
Murtadha <202203868@student.polytechnic.bh>
2025-12-15 16:15:01 +0300

    add page profile

---
+++ Masar/ProfilePageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfilePageViewController.swift
+//  Masar
+//
+//  Created by BP-36-201-10 on 15/12/2025.
+//
+
+import UIKit
+
+class ProfilePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
