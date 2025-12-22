9fff1ee1a7ba03c5c09edaabdbf613d979fd077f
n22098 <202201259@student.polytechnic.bh>
2025-12-22 14:29:56 +0300

    Making the stars effective

---
+++ Masar/BookingHistoryCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookingHistoryCell.swift
+//  Masar
+//
+//  Created by BP-36-212-14 on 22/12/2025.
+//
+
+import UIKit
+
+class BookingHistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Masar/BookingModel.swift
@@ -0,0 +1,8 @@
+//
+//  BookingModel.swift
+//  Masar
+//
+//  Created by BP-36-212-14 on 22/12/2025.
+//
+
+import Foundation
