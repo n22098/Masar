926a3bd8b49dfde015918ee247872e6ef306c3a4
202304651 <202304651@student.polytechnic.bh>
2025-12-23 13:24:53 +0300

    add providor hub button

--- Masar/SceneDelegate.swift
+++ Masar/SceneDelegate.swift
@@ -41,14 +41,70 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let profileVC = ProfileViewController()
         let profileNav = UINavigationController(rootViewController: profileVC)
         profileNav.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
+        let providerHubVC = UIViewController()
+        let providerHubNav = UINavigationController(rootViewController: providerHubVC)
+        providerHubNav.tabBarItem = UITabBarItem(
+            title: "Provider Hub",
+            image: UIImage(systemName: "briefcase"),
+            selectedImage: UIImage(systemName: "briefcase.fill")
+        )
+        searchNav.tabBarItem = UITabBarItem(
+            title: "Search",
+            image: UIImage(systemName: "magnifyingglass"),
+            selectedImage: UIImage(systemName: "magnifyingglass")
+        )
+
+        historyNav.tabBarItem = UITabBarItem(
+            title: "History",
+            image: UIImage(systemName: "clock"),
+            selectedImage: UIImage(systemName: "clock.fill")
+        )
+
+        messagesNav.tabBarItem = UITabBarItem(
+            title: "Messages",
+            image: UIImage(systemName: "message"),
+            selectedImage: UIImage(systemName: "message.fill")
+        )
+
+        profileNav.tabBarItem = UITabBarItem(
+            title: "Profile",
+            image: UIImage(systemName: "person"),
+            selectedImage: UIImage(systemName: "person.fill")
+        )
 
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [
             searchNav,
             historyNav,
             messagesNav,
+            providerHubNav,
             profileNav
         ]
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .white
+
+        appearance.stackedLayoutAppearance.selected.iconColor =
+            UIColor(red: 112/255, green: 79/255, blue: 217/255, alpha: 1)
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [
+            .foregroundColor: UIColor(red: 112/255, green: 79/255, blue: 217/255, alpha: 1)
+        ]
+
+        appearance.stackedLayoutAppearance.normal.iconColor = .systemGray
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [
+            .foregroundColor: UIColor.systemGray
+        ]
+
+        // Remove top shadow line
+        appearance.shadowColor = nil
+
+        let tabBar = tabBarController.tabBar
+        tabBar.standardAppearance = appearance
+
+        if #available(iOS 15.0, *) {
+            tabBar.scrollEdgeAppearance = appearance
+        }
+
 
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
