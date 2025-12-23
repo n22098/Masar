aa92d37d99c4ee75a3c26de93156db37c7e3d385
n22098 <202201259@student.polytechnic.bh>
2025-12-23 14:58:46 +0300

    make the rating appear

---
+++ Masar/RatingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RatingTableViewCell.swift
+//  Masar
+//
+//  Created by Guest User on 23/12/2025.
+//
+
+import UIKit
+
+class RatingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Masar/RatingsReviewsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RatingsReviewsViewController.swift
+//  Masar
+//
+//  Created by Guest User on 23/12/2025.
+//
+
+import UIKit
+
+class RatingsReviewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
